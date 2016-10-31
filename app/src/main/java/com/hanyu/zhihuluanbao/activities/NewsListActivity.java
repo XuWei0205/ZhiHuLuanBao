32f0dfb284a854b215f97de752beacdb5209b496
Hanyu <815624804@qq.com>
2016-10-31 23:46:15 +0800

    add the function that can refresh the news and  some bugs hasbeen found and handled

--- app/src/main/java/com/hanyu/zhihuluanbao/activities/NewsListActivity.java
+++ app/src/main/java/com/hanyu/zhihuluanbao/activities/NewsListActivity.java
@@ -26,6 +26,7 @@ import com.hanyu.zhihuluanbao.models.NewsListModel;
 import com.hanyu.zhihuluanbao.models.StoryModel;
 import com.hanyu.zhihuluanbao.utils.CLog;
 import com.hanyu.zhihuluanbao.utils.Debug;
+import com.hanyu.zhihuluanbao.utils.LocalDisplay;
 import com.hanyu.zhihuluanbao.utils.Util;
 import com.hanyu.zhihuluanbao.views.BannerView;
 
@@ -35,6 +36,11 @@ import java.util.Calendar;
 import java.util.List;
 
 import cn.trinea.android.view.autoscrollviewpager.AutoScrollViewPager;
+import in.srain.cube.views.ptr.PtrClassicFrameLayout;
+import in.srain.cube.views.ptr.PtrDefaultHandler;
+import in.srain.cube.views.ptr.PtrFrameLayout;
+import in.srain.cube.views.ptr.PtrHandler;
+import in.srain.cube.views.ptr.header.MaterialHeader;
 
 /**
  * Created by Dell on 2016/10/21.
@@ -60,6 +66,7 @@ public class NewsListActivity extends BasicActivity implements View.OnClickListe
 
     private AutoScrollViewPager viewPager;
     private ViewPageAdapter pageAdapter;
+    private PtrFrameLayout mPtrFrame;
 
 
 
@@ -79,7 +86,7 @@ public class NewsListActivity extends BasicActivity implements View.OnClickListe
         dateFormat = new SimpleDateFormat("yyyyMMdd");
         date = dateFormat.format(now.getTime());
         CLog.i("date--------> ", "" + date);
-        getData(date);
+        getData(date,false);
         headerView=LayoutInflater.from(getApplicationContext())
                 .inflate(R.layout.news_list_header_layout, null);
         listView.addHeaderView(headerView);
@@ -122,7 +129,7 @@ public class NewsListActivity extends BasicActivity implements View.OnClickListe
                         lastVisibleIndex == newsAdapter.getCount() + 1) {
                     CLog.i(newsAdapter.getCount() + "----------------" + lastVisibleIndex);
                     progressBar.setVisibility(View.VISIBLE);
-                    getData(date);
+                    getData(date,false);
                     CLog.i("BeforeData------->", date);
                 }
 
@@ -133,17 +140,71 @@ public class NewsListActivity extends BasicActivity implements View.OnClickListe
         });
 
 
+
+        /** 设置mPtrFrame **/
+        mPtrFrame = (PtrFrameLayout) findViewById(R.id.list_frame);
+        final MaterialHeader header = new MaterialHeader(getApplicationContext());
+        int[] colors = getResources().getIntArray(R.array.google_colors);
+        header.setColorSchemeColors(colors);
+        header.setLayoutParams(new PtrFrameLayout.LayoutParams(-1, -2));
+        header.setPadding(0, LocalDisplay.dp2px(15), 0, LocalDisplay.dp2px(10));
+        header.setPtrFrameLayout(mPtrFrame);
+
+
+
+
+        mPtrFrame.setLoadingMinTime(1000);
+        mPtrFrame.setDurationToCloseHeader(1500);
+        mPtrFrame.setHeaderView(header);
+        mPtrFrame.addPtrUIHandler(header);
+        mPtrFrame.setResistance(2.0f);//设置提抗力
+        mPtrFrame.setRatioOfHeaderHeightToRefresh(0.2f);//触发刷新时移动的位置比例
+        mPtrFrame.setDurationToClose(200);//回弹延时
+        mPtrFrame.setDurationToCloseHeader(1000);//头部回弹时间
+        mPtrFrame.setPullToRefresh(false);//
+        mPtrFrame.setKeepHeaderWhenRefresh(true);//
+//        mPtrFrame.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                mPtrFrame.autoRefresh();
+//            }
+//        }, 100);
+
+
+        mPtrFrame.setPtrHandler(new PtrHandler() {
+            @Override
+            public boolean checkCanDoRefresh(PtrFrameLayout frame, View content, View header) {
+                return PtrDefaultHandler.checkContentCanBePulledDown(frame, content, header);
+            }
+
+            @Override
+            public void onRefreshBegin(PtrFrameLayout frame) {
+                CLog.i("do refresh");
+                isLatest = true;
+
+                getData(date,true);
+
+            }
+        });
+
+
+
+
     }
 
 
 
-    private void getData(final String dates){
+    private void getData(final String dates, final boolean isFromRefresh){
         String url;
-        if (isLatest) {
+        if (isLatest ) {
             isLatest = false;
             url=API.GET_LATEST_NEWS_URL;
         }else{
-            url = API.BASE_GET_BEFORE_URL + dates;
+
+                url = API.BASE_GET_BEFORE_URL + dates;
+                now.add(Calendar.DATE, -1);
+                date = dateFormat.format(now.getTime());
+
         }
         NetManager.doHttpGet(getApplicationContext(), null, url, null,
                 NewsListModel.class, new NetManager.ResponseListener<NewsListModel>() {
@@ -153,6 +214,9 @@ public class NewsListActivity extends BasicActivity implements View.OnClickListe
                             // 设置banner
                             if (viewList == null) {
                                 viewList=new ArrayList<BannerView>(response.top_stories.size());
+                            }
+                            if (isFromRefresh) {
+                                viewList.clear();
                             }
                             BannerView bannerView;
                             for (int i = 0; i < response.top_stories.size(); i++) {
@@ -166,23 +230,42 @@ public class NewsListActivity extends BasicActivity implements View.OnClickListe
                             if (pageAdapter == null) {
                                 pageAdapter = new ViewPageAdapter(viewList);
                                 viewPager.setAdapter(pageAdapter);
+                            }else{
+
+                                pageAdapter.notifyDataSetChanged();
                             }
                         }
                         newsDate.setText(response.date);
-                        if (response.stories != null) {
+                        if (response.stories != null ) {
+                            if (isFromRefresh){
+                                allDatas.clear();
+                            }
                             allDatas.addAll(response.stories);
                             newsAdapter.setDatas(allDatas);
                             newsAdapter.notifyDataSetChanged();
-                            now.add(Calendar.DATE, -1);
-                            date = dateFormat.format(now.getTime());
+
+
+                            if (isFromRefresh){
+                                now = Calendar.getInstance();
+                                date = dateFormat.format(now.getTime());
+
+                            }
                         } else {
                             Util.toastTips(getApplicationContext(), "读取数据失败");
+                        }
+                        if(isFromRefresh){
+                            mPtrFrame.refreshComplete();
                         }
 
                     }
 
                     @Override
                     public void onErrorResponse(VolleyError error) {
+                        if (isFromRefresh){
+                            mPtrFrame.refreshComplete();
+                        }
+                        Util.toastTips(getApplicationContext(),"加载失败");
+
 
                     }
 
